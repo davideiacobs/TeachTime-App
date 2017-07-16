44ea0b4414a11ab7e7b8f321431866fac99b49f1
davideiacobs@gmail.com <davideiacobs@gmail.com>
2017-07-16 18:48:07 +0200

    problema risolto

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ export const USE_PROXY = true;
 
 export const URL_BASE = USE_PROXY ? 'rest/' : 'http://localhost:8080/teachTime/MainApplication/rest/';
 
-export const MY_URL_BASE = "http://localhost:8080/teachTime/MainApplication/rest/";
+export const MY_URL_BASE = "http://192.168.1.5:8080/teachTime/MainApplication/rest/";
 
-//http://192.168.1.5:8080/teachTime/MainApplication/rest/
+
 export const URL = {
     /*USERS: {
         SIGNUP: "users/",
